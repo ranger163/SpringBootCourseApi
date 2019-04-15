4de9aabe7e9a3ce28cb14caccfb473c0c5af08cf
Ahmed Nassar <ahmed.nassar163@gmail.com>
2019-04-15 11:30:04 +0200

    Adding a Rest Controller Adding hello class Returning objects from controller

---
+++ course-api/src/main/java/me/inassar/springbootstarter/topic/TopicController.java
@@ -0,0 +1,21 @@
+package me.inassar.springbootstarter.topic;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TopicController {
+
+	@RequestMapping("/topics")
+	public List<TopicModel> getAllTopics() {
+		return Arrays.asList(
+				new TopicModel("Spring", "Spring FrameWork", "Spring Framework Description"),
+				new TopicModel("Java", "Core Java", "Core Java Description"),
+				new TopicModel("Javascript", "Javascript", "Javascript description")
+				);
+	}
+
+}
