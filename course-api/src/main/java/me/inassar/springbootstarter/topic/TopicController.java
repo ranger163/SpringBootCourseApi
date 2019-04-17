006dd9637173aaa756ca6de6372b2ac531f819a4
Ahmed Nassar <ahmed.nassar163@gmail.com>
2019-04-17 13:58:21 +0200

    Updating a resource Deleting a resource

--- course-api/src/main/java/me/inassar/springbootstarter/topic/TopicController.java
+++ course-api/src/main/java/me/inassar/springbootstarter/topic/TopicController.java
@@ -8,22 +8,39 @@ import java.util.List;
 @RestController
 public class TopicController {
 
+    // Injecting topics service object
     @Autowired
     private TopicService topicService;
 
+    // Getting all topics
     @RequestMapping("/topics")
     public List<TopicModel> getAllTopics() {
         return topicService.getTopics();
     }
 
+    // Getting a specific topic using it's id
     @RequestMapping("/topics/{id}")
     public TopicModel getTopic(@PathVariable String id) {
         return topicService.getTopic(id);
     }
 
+    // Adding new topic to old records
     @RequestMapping(method = RequestMethod.POST, value = "/topics")
     public void addTopic(@RequestBody TopicModel topic) {
         topicService.addTopic(topic);
     }
+
+    // Updating a topic using it's id
+    @RequestMapping(method = RequestMethod.PUT, value = "/topics/{id}")
+    public void updateTopic(@RequestBody TopicModel topic, @PathVariable String id) {
+        topicService.updateTopic(id, topic);
+    }
+
+    // Deleting a topic using it's id
+    @RequestMapping(method = RequestMethod.DELETE, path = "/topics/{id}")
+    public void deleteTopic(@PathVariable String id) {
+        topicService.deleteTopic(id);
+    }
+
 
 }
--- course-api/src/main/java/me/inassar/springbootstarter/topic/TopicService.java
+++ course-api/src/main/java/me/inassar/springbootstarter/topic/TopicService.java
@@ -9,21 +9,41 @@ import java.util.List;
 @Service
 public class TopicService {
 
+    // Mocking records
     private List<TopicModel> topics = new ArrayList<>(Arrays.asList(
             new TopicModel("Spring Boot", "Spring Boot FrameWork", "Spring Framework Description"),
             new TopicModel("Java", "Core Java", "Core Java Description"),
             new TopicModel("Javascript", "Javascript", "Javascript description")
     ));
 
+    // Getting all topics from DB
     List<TopicModel> getTopics() {
         return topics;
     }
 
+    // Getting a specific topic using it's id
     TopicModel getTopic(String id) {
         return topics.stream().filter(topic -> topic.getId().equalsIgnoreCase(id)).findFirst().get();
     }
 
+    // Adding new topic to old records
     void addTopic(TopicModel topic) {
         topics.add(topic);
+    }
+
+    // Updating a topic using it's id
+    void updateTopic(String id, TopicModel topic) {
+        for (int i = 0; i < topics.size(); i++) {
+            TopicModel topicModel = topics.get(i);
+            if (topicModel.getId().equalsIgnoreCase(id)) {
+                topics.set(i, topic);
+                return;
+            }
+        }
+    }
+
+    // Delete topic using it's id
+    void deleteTopic(String id) {
+        topics.removeIf(topic -> topic.getId().equalsIgnoreCase(id));
     }
 }
