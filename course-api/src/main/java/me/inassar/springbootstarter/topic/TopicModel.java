63fd77503fdb8c054750b623aa0acad2ba1786f6
Ahmed Nassar <ahmed.nassar163@gmail.com>
2019-04-18 14:54:17 +0200

    Creating a Spring Data JPA Repository

--- course-api/src/main/java/me/inassar/springbootstarter/topic/TopicModel.java
+++ course-api/src/main/java/me/inassar/springbootstarter/topic/TopicModel.java
@@ -1,44 +1,49 @@
 package me.inassar.springbootstarter.topic;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class TopicModel {
 
-	private String id;
-	private String name;
-	private String description;
-	
-	public TopicModel() {
-		
-	}
+    @Id
+    private String id;
+    private String name;
+    private String description;
 
-	public TopicModel(String id, String name, String description) {
-		super();
-		this.id = id;
-		this.name = name;
-		this.description = description;
-	}
+    public TopicModel() {
 
-	public String getId() {
-		return id;
-	}
+    }
 
-	public void setId(String id) {
-		this.id = id;
-	}
+    TopicModel(String id, String name, String description) {
+        super();
+        this.id = id;
+        this.name = name;
+        this.description = description;
+    }
 
-	public String getName() {
-		return name;
-	}
+    String getId() {
+        return id;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    void setId(String id) {
+        this.id = id;
+    }
 
-	public String getDescription() {
-		return description;
-	}
+    String getName() {
+        return name;
+    }
 
-	public void setDescription(String description) {
-		this.description = description;
-	}
+    void setName(String name) {
+        this.name = name;
+    }
+
+    String getDescription() {
+        return description;
+    }
+
+    void setDescription(String description) {
+        this.description = description;
+    }
 
 }
---
+++ course-api/src/main/java/me/inassar/springbootstarter/topic/TopicRepository.java
@@ -0,0 +1,7 @@
+package me.inassar.springbootstarter.topic;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface TopicRepository extends CrudRepository<TopicModel, String> {
+
+}
