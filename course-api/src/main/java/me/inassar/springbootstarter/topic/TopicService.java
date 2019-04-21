e58b8b8066be3d2c4177ee676c5f7b8630242d23
Ahmed Nassar <ahmed.nassar163@gmail.com>
2019-04-21 10:36:32 +0200

    Making Crud Operations With Repository "Create, Read, Update & Delete"

--- course-api/src/main/java/me/inassar/springbootstarter/topic/TopicService.java
+++ course-api/src/main/java/me/inassar/springbootstarter/topic/TopicService.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 @Service
@@ -12,13 +11,6 @@ public class TopicService {
 
     @Autowired
     private TopicRepository topicRepository;
-
-    // Mocking records
-    private List<TopicModel> topics = new ArrayList<>(Arrays.asList(
-            new TopicModel("Spring Boot", "Spring Boot FrameWork", "Spring Framework Description"),
-            new TopicModel("Java", "Core Java", "Core Java Description"),
-            new TopicModel("Javascript", "Javascript", "Javascript description")
-    ));
 
     // Getting all topics from DB
     List<TopicModel> getTopics() {
@@ -29,7 +21,7 @@ public class TopicService {
 
     // Getting a specific topic using it's id
     TopicModel getTopic(String id) {
-        return topics.stream().filter(topic -> topic.getId().equalsIgnoreCase(id)).findFirst().get();
+        return topicRepository.findOne(id);
     }
 
     // Adding new topic to old records
@@ -39,18 +31,11 @@ public class TopicService {
 
     // Updating a topic using it's id
     void updateTopic(String id, TopicModel topic) {
-        for (int i = 0; i < topics.size(); i++) {
-            TopicModel topicModel = topics.get(i);
-            if (topicModel.getId().equalsIgnoreCase(id)) {
-                topics.set(i, topic);
-                return;
-            }
-        }
+        topicRepository.save(topic);
     }
 
     // Delete topic using it's id
     void deleteTopic(String id) {
         topicRepository.delete(id);
-        topics.removeIf(topic -> topic.getId().equalsIgnoreCase(id));
     }
 }
